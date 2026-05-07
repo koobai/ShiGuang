d8ac68267e5911a97e060eb74a3a669e503a79da
koobai <mekoobai@gmail.com>
2026-05-07 17:56:01 +0800

    Initial Commit

---
+++ DisplayBrightness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DisplayBrightness
+//
+//  Created by koobai on 2026/5/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DisplayBrightness/DisplayBrightnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayBrightnessApp.swift
+//  DisplayBrightness
+//
+//  Created by koobai on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct DisplayBrightnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
